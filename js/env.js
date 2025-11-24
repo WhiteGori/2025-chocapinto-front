eff11ee1a64449ffb33f6d0544e1aabd7db26d4d
WhiteGori <juanmblancog@gmail.com>
2025-11-24 18:48:54 -0300

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
-//export const API_URL = "https://two025-chocapinto-back.onrender.com";
+export const API_URL = "https://two025-chocapinto-back.onrender.com";
 // env.js (desarrollo rápido)
-export const API_URL = "http://localhost:5000";
+//export const API_URL = "http://localhost:5000";
 
